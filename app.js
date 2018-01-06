41303fbca2d5d60aac7bb56aa3c186a0b8bb67fa
AndreBtt <Bittencourtandre@hotmail.com>
2018-01-06 01:09:43 -0200

    Codigo totalmente refatorado, com padrao MVC

--- app.js
+++ app.js
@@ -1,53 +1,6 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var firebase = require('firebase');
+var app = require('./config/server');
+var firebase = require('./config/dbConnection');
 
-var config = {
-    apiKey: "AIzaSyAimq7NMwJEhpmLrOTCZSU6X48QzK2oCiQ",
-    authDomain: "alfredo-692b8.firebaseapp.com",
-    databaseURL: "https://alfredo-692b8.firebaseio.com",
-    projectId: "alfredo-692b8",
-    storageBucket: "alfredo-692b8.appspot.com",
-    messagingSenderId: "599701048232"
-};
-firebase.initializeApp(config);
-
-var index = require('./routes/index');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.listen(8000, function(req,res){
+	console.log("servidor On na porta 3000");
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
