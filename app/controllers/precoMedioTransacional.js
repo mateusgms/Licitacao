464cca237c9f3f275c0f1ce2fc9ac2a561dd8ecc
AndreBtt <bittencourtandre@hotmail.com>
2018-02-23 16:04:14 -0300

    sem internet

--- app/controllers/precoMedioTransacional.js
+++ app/controllers/precoMedioTransacional.js
@@ -7,59 +7,62 @@ module.exports.index = function(app,req,res){
     //     res.render('login');
     // }
 
-    var produtos = JSON.parse(fs.readFileSync("precoMedioTransacional.json"));
+    // var produtos = JSON.parse(fs.readFileSync("precoMedioTransacional.json"));
 
-    var email = firebase.auth().currentUser.email.toString();
+    // var email = firebase.auth().currentUser.email.toString();
 
-    /*acessa o banco de dados e carrega o dados em carrinho */
-    var busca = firebase.database().ref('/');
+    // /*acessa o banco de dados e carrega o dados em carrinho */
+    // var busca = firebase.database().ref('/');
 
-    var chave = "precoMedioTransacional";
+    // var chave = "precoMedioTransacional";
 
-    busca.once('value')
-        .then(function (snap) {
-            var codigos = [];
-            var quantidades = [];
-            var precos = [];
-            var regioes = [];
-            for (var key in snap.val()) {
-                var elemento = snap.val()[key];
-                if (elemento.email.toString() === email && elemento[chave] !== undefined) {
-                    codigos = elemento[chave].codigo;
-                    quantidades = elemento[chave].quantidade;
-                    precos = elemento[chave].preco;
-                    regioes = elemento[chave].regiao;
-                    break;
-                }
-            }
+    // busca.once('value')
+    //     .then(function (snap) {
+    //         var codigos = [];
+    //         var quantidades = [];
+    //         var precos = [];
+    //         var regioes = [];
+    //         for (var key in snap.val()) {
+    //             var elemento = snap.val()[key];
+    //             if (elemento.email.toString() === email && elemento[chave] !== undefined) {
+    //                 codigos = elemento[chave].codigo;
+    //                 quantidades = elemento[chave].quantidade;
+    //                 precos = elemento[chave].preco;
+    //                 regioes = elemento[chave].regiao;
+    //                 break;
+    //             }
+    //         }
 
-            var carrinho = [];
+    //         var carrinho = [];
 
-            for (var i = 0; i < codigos.length; i++) {
-                for (var j = 0; j < produtos.length; j++) {
-                    if (produtos[j]["CODIGO"].toString().trim() === codigos[i].toString().trim()
-                        && produtos[j]["REGIAO"].toString().trim() === regioes[i].toString().trim()) {
-                        var novo = {
-                            codigo: produtos[j]['CODIGO'],
-                            nome: produtos[j]['NOME'],
-                            vencimento: produtos[j]['DATA'],
-                            unidade: produtos[j]['UNIDADE'],
-                            regiao: regioes[i],
-                            especificacao: produtos[j]['DESCRICAO'],
-                            qtdp: produtos[j]['QUANTIDADE'],
-                            precoMinimo: parseFloat(produtos[j]['MINIMO']).toFixed(2),
-                            precoMedio: parseFloat(produtos[j]['MEDIA']).toFixed(2),
-                            precoMaximo: parseFloat(produtos[j]['MAXIMO']).toFixed(2),
-                            precoFinal: parseFloat(precos[i]).toFixed(2),
-                            quantidade: quantidades[i]
-                        };
-                        carrinho.push(novo);
-                    }
-                }
-            }
+    //         for (var i = 0; i < codigos.length; i++) {
+    //             for (var j = 0; j < produtos.length; j++) {
+    //                 if (produtos[j]["CODIGO"].toString().trim() === codigos[i].toString().trim()
+    //                     && produtos[j]["REGIAO"].toString().trim() === regioes[i].toString().trim()) {
+    //                     var novo = {
+    //                         codigo: produtos[j]['CODIGO'],
+    //                         nome: produtos[j]['NOME'],
+    //                         vencimento: produtos[j]['DATA'],
+    //                         unidade: produtos[j]['UNIDADE'],
+    //                         regiao: regioes[i],
+    //                         especificacao: produtos[j]['DESCRICAO'],
+    //                         qtdp: produtos[j]['QUANTIDADE'],
+    //                         precoMinimo: parseFloat(produtos[j]['MINIMO']).toFixed(2),
+    //                         precoMedio: parseFloat(produtos[j]['MEDIA']).toFixed(2),
+    //                         precoMaximo: parseFloat(produtos[j]['MAXIMO']).toFixed(2),
+    //                         precoFinal: parseFloat(precos[i]).toFixed(2),
+    //                         quantidade: quantidades[i]
+    //                     };
+    //                     carrinho.push(novo);
+    //                 }
+    //             }
+    //         }
 
-            res.render('PrecoMedioTransacional/home', {carrinho: carrinho});
-        });
+    //         res.render('PrecoMedioTransacional/home', {carrinho: carrinho});
+    //     });
+
+    res.render('PrecoMedioTransacional/home', {carrinho: []});
+
 }
 
 module.exports.gerarOrcamento = function(app,req,res){
